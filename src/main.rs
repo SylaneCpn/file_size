3d35bc16cc1600798b8e338303ad4dca42fc4bc0
SylaneCpn <sylane.campan@gmail.com>
2024-11-01 08:53:48 -0400

    mode + fmt

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
 use std::fs;
+use clap::Parser;
+
+
+/// Simple program to list the size of the files in the specified path
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Print mode : 0 => terminal, 1 => file
+    #[arg(short, long,default_value_t = 0)]
+    mode : u8,
+    /// Path to scan
+    path: String,
+}
 
 #[derive(Debug)]
 struct Size {
@@ -9,16 +22,18 @@ struct Size {
 
 fn main() -> std::io::Result<()> {
     // collect args
-    let args: Vec<_> = std::env::args().collect();
+    let args = Cli::parse();
+
+    let mode = args.mode;
 
     // last arg is the path to scan
-    let path = &args[args.len() - 1];
+    let path = args.path;
 
     //result
-    let sizes = folder_size(path)?;
+    let mut sizes = folder_size(&path)?;
 
     //display result somewhere
-    size_display(&sizes);
+    size_display(&mut sizes,mode)?;
 
     Ok(())
 }
@@ -66,8 +81,61 @@ fn folder_size(path: &str) -> std::io::Result<Vec<Size>> {
     Ok(sizes)
 }
 
-fn size_display(sizes: &[Size]) {
+fn size_display(sizes: &mut[Size],mode : u8) -> std::io::Result<()> {
+    sizes.sort_by_key(|k| k.size);
+    let mut to_write = String::new();
+    
     sizes.iter().for_each(|x| {
-        println!("{:?}", x);
+        to_write = format!("{}{}\t{}\t{}\n",to_write,if x.is_file {"F"} else {"DIR"}, &x.name , size_unit(x.size) );
     });
+
+    if mode == 0 {
+        print!("{}",to_write);
+    }
+
+    else {
+        fs::write("size.txt", to_write.as_bytes())?;
+    }
+
+    Ok(())
+}
+
+fn size_unit(size : u64 ) -> String {
+    let mut dept = 0;
+    let original_size = size as f64;
+    let mut s = original_size;
+
+    loop {
+
+        if s/1000.0 >= 1.0  {
+            s /= 1000.0;
+            dept = dept + 1;
+        }
+
+        else {
+            break;
+        }
+  
+    }
+
+    if dept == 0 {
+        format!("{} b",original_size)
+
+    }
+
+    else if dept == 1 {
+        format!("{} Kb",original_size / 1_000.0)
+    }
+
+    else if dept == 2 {
+        format!("{} Mb",original_size / 1_000_000.0)
+    }
+
+    else if dept == 3 {
+        format!("{} Gb",original_size / 1_000_000_000.0)
+    }
+
+    else {
+        format!("{} Tb",original_size / 1_000_000_000_000.0)
+    }
 }
